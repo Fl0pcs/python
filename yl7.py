fac70e5b2aef109a4900e32b1adddd2232c621a7
rainis <rainis.toming@ametikool.ee>
2025-11-20 16:59:55 +0200

    yl 7

---
+++ yl7.py
@@ -0,0 +1,8 @@
+#Kirjuta programm, mis ütleb, kas kasutaja poolt etteantud täisarv 
+# on paarisarv või mitte. (paarisarvu mõiste - odd/even)
+
+number = int(input("number: "))
+if number % 2 == 0:
+    print("Number on paaris.")
+else:
+     print("Number on paaritu.")
